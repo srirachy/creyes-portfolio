df20dcd7fd9d29e0dd815a767d91b3714463c0ce
srirachy <caseyereyes@gmail.com>
2023-05-15 12:18:04 -0700

    perf(detect browser): render tech component on browser only

--- client/src/components/Tech.jsx
+++ client/src/components/Tech.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { isFirefox } from 'react-device-detect';
+import { isFirefox, isBrowser } from 'react-device-detect';
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
@@ -7,19 +7,22 @@ import { technologies } from "../constants";
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {isFirefox ? 
-        technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))
-      :
-        technologies.slice(0, 13).map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))
+      { isBrowser &&
+        (isFirefox ? 
+          technologies.map((technology) => (
+            <div className="w-28 h-28" key={technology.name}>
+              <BallCanvas icon={technology.icon} />
+            </div>
+          ))
+        :
+          technologies.slice(0, 13).map((technology) => (
+            <div className="w-28 h-28" key={technology.name}>
+              <BallCanvas icon={technology.icon} />
+            </div>
+          ))
+        )
       }
+      
     </div>
   )
 }
