1c09f6bc64e60b3918b2d42e383b8832ec72d9e1
srirachy <caseyereyes@gmail.com>
2023-05-14 19:57:17 -0700

    perf(add route): add initial route for deployment

--- server/index.js
+++ server/index.js
@@ -10,12 +10,15 @@ const PORT = process.env.PORT || 8000;
 app.use(jsonParser);
 app.use(cors());
 
+app.get('/', (_, res) => {
+  res.send('Portfolio server running');
+});
+
 app.post("/verify-token", async (req, res) => {
   const { reCAPTCHA_TOKEN, Secret_Key } = req.body;
 
   try {
     let response = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${Secret_Key}&response=${reCAPTCHA_TOKEN}`);
-    console.log(response.data);
 
     return res.status(200).json({
       success: true,
@@ -23,8 +26,6 @@ app.post("/verify-token", async (req, res) => {
       verificationInfo: response.data,
     });
   } catch(err) {
-    console.log(err);
-
     return res.status(500).json({
       success: false,
       message: "Error verifying token",
